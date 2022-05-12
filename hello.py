0a60143e950aa505e1879ebf88551c2a5194368c
Wuzc1996 <1510328918@qq.com>
2022-05-12 15:11:50 +0800

    测试

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+import requests
+URL = 'http://c.biancheng.net/uploads/course/python_spider/191009.html'
+# 输入在浏览器的网址
+res = requests.get(URL)
+res.encoding = 'bg2312'
+# 发送 GET 方式的请求，并把返回的结果(响应)存储在 res 变量里头
+# 答第二个问题，get() 方法需要输入一个网页链接
+print(res.text)
+# print('中文乱码')
---
+++ sqlwork.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Plasma.Archives WHERE Id = 'BC0CC304-B342-EC11-8D99-ECB1D77E9DEB';
+
+SELECT * FROM Plasma.TempBloodSamples WHERE Created > '2022-05-01 00:00:00.00';
+
+SELECT * FROM Plasma.ApplyRecords WHERE Created > '2022-05-01 00:00:00.00';
+
+SELECT * FROM Plasma.RejectPlasmaReasons WHERE Created > '2022-05-01 00:00:00.00';
+
+SELECT * FROM Plasma.DeliverRecords WHERE Created > '2022-04-01 00:00:00.00';
+
+SELECT * FROM Plasma.Message
