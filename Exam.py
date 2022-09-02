45bd7656f078cb23b35ec8bfc58e19ec350c05f2
Wuzc1996 <1510328918@qq.com>
2022-09-02 09:56:37 +0800

    修正新浆员登记体检信息

--- Exam.py
+++ Exam.py
@@ -1,10 +1,10 @@
 import random
 import time
+
 import requests
 
 from Login import login
 from sqlcont import sqlselect
-
 
 day = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 headers = login()
@@ -25,42 +25,33 @@ where datediff(dd, pr.Created, sysdatetime()) = 0
 for i in range(0, len(examlist)):
     id = str(examlist[i][1])
     sql = "select top 1 py.Weight, py.Temperature, py.Pulse, py.SBP, py.DBP from Plasma.Archives pa left join Plasma.Registrations pr on pa.Id = pr.ArchiveId left join Plasma.PhysicalExams py on py.Id = pr.ExamResultId where pa.Id = {} and pr.ExamResultId is not null order by pr.Created desc".format(
-        "'"+id+"'")
+        "'" + id + "'")
 
     LastExam = sqlselect(sql)
-    print(LastExam)
-
-    body = "ReturnToIntradayPlysicalExam=False&IDNum={0}&" \
-           "Registration.Archive.PersonalInfo.PictureBase64=&Registration.Archive.PersonalInfo.IDNum={1}&" \
-           "Registration.Archive.PersonalInfo.Gender={2}&Registration.SupplyNum={3}&" \
-           "DateCollected={4}&Weight={5}&Temperature={6}&Pulse={7}&SBP={8}&DBP={9}&CardioPulmonary=1&" \
-           "Skin=1&ENT=1&Hepatolienal=1&Limbs=1&others=&Result=0&Doctor=陆艳春&PermittedDoctor=林慧莲&" \
-           "Registration.Id={10}&LastWeight={11}&LastTemp={12}&LastPulse={13}&" \
-           "LastSBP={14}&LastDBP={15}&Score=&physicalInfo.Archive.Id={16}".format(examlist[i][0],
-                                                                                  examlist[i][0],
-                                                                                  examlist[i][4],
-                                                                                  examlist[i][3],
-                                                                                  day,
-                                                                                  random.randint(int(LastExam[0]) - 1,
-                                                                                                 int(LastExam[0]) + 1),
-                                                                                  round(random.uniform(
-                                                                                      round(LastExam[1], 2) - 0.5,
-                                                                                      round(LastExam[1], 2) + 0.5), 1),
-                                                                                  random.randint(int(LastExam[2]) - 5,
-                                                                                                 int(LastExam[2]) + 5),
-                                                                                  random.randint(int(LastExam[3]) - 5,
-                                                                                                 int(LastExam[3]) + 5),
-                                                                                  random.randint(int(LastExam[4]) - 5,
-                                                                                                 int(LastExam[4]) + 5),
-                                                                                  examlist[i][2],
-                                                                                  int(LastExam[0]),
-                                                                                  round(LastExam[1], 1),
-                                                                                  int(LastExam[2]),
-                                                                                  int(LastExam[3]),
-                                                                                  int(LastExam[4]),
-                                                                                  examlist[i][1]
-                                                                                  )
+    body = ''
+    if len(LastExam) == 0:
+        body = "ReturnToIntradayPlysicalExam=False&IDNum={0}&Registration.Archive.PersonalInfo.PictureBase64=&" \
+               "Registration.Archive.PersonalInfo.IDNum={0}&Registration.Archive.PersonalInfo.Gender={1}&" \
+               "Registration.SupplyNum={2}&DateCollected={3}&Weight={4}&Temperature={5}&" \
+               "Pulse={6}&SBP={7}&DBP={8}&CardioPulmonary=1&Skin=1&ENT=1&Hepatolienal=1&Limbs=1&others=正常&Result=0&" \
+               "Doctor=陆艳春&PermittedDoctor=韦国勇&AutoCreateMedicalRecord=true&" \
+               "Registration.Id={9}&Score=&physicalInfo.Archive.Id={10}".format(
+            examlist[i][0], examlist[i][4], examlist[i][3], day, random.randint(55, 70),
+            round(random.uniform(36.1, 37.1), 1), random.randint(70, 100), random.randint(100, 130),
+            random.randint(60, 70), examlist[i][2], examlist[i][1])
+    else:
+        LastExam = LastExam[0]
+        body = "ReturnToIntradayPlysicalExam=False&IDNum={0}&" \
+               "Registration.Archive.PersonalInfo.PictureBase64=&Registration.Archive.PersonalInfo.IDNum={1}&" \
+               "Registration.Archive.PersonalInfo.Gender={2}&Registration.SupplyNum={3}&" \
+               "DateCollected={4}&Weight={5}&Temperature={6}&Pulse={7}&SBP={8}&DBP={9}&CardioPulmonary=1&" \
+               "Skin=1&ENT=1&Hepatolienal=1&Limbs=1&others=&Result=0&Doctor=陆艳春&PermittedDoctor=林慧莲&" \
+               "Registration.Id={10}&LastWeight={11}&LastTemp={12}&LastPulse={13}&" \
+               "LastSBP={14}&LastDBP={15}&Score=&physicalInfo.Archive.Id={16}".format(examlist[i][0], examlist[i][0],
+            examlist[i][4], examlist[i][3], day, random.randint(int(LastExam[0]) - 1, int(LastExam[0]) + 1),
+            round(random.uniform(round(LastExam[1], 2) - 0.5, round(LastExam[1], 2) + 0.5), 1),
+            random.randint(int(LastExam[2]) - 5, int(LastExam[2]) + 5), random.randint(int(LastExam[3]) - 5,int(LastExam[3]) + 5),
+            random.randint(int(LastExam[4]) - 5, int(LastExam[4]) + 5), examlist[i][2], int(LastExam[0]),
+            round(LastExam[1], 1), int(LastExam[2]), int(LastExam[3]), int(LastExam[4]), examlist[i][1])
     r = requests.post(ExamUrl, data=body.encode('utf-8'), headers=headers, allow_redirects=False)
     print(r.text)
-
-
